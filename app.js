fb9bee1151dede5056097fcfe9bc62162db142a4
Sourav29pal <souravjnv29@gmail.com>
2025-08-19 00:01:56 +0530

    some changees

--- app.js
+++ app.js
@@ -66,9 +66,9 @@ const sessionOptions = {
   },
 };
 
-// app.get("/", (req, res) => {
-//   res.send("Home Page");
-// });
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -98,7 +98,6 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong!" } = err;
   res.status(statusCode).render("error.ejs", { message });
-  // res.status(statusCode).send(message);
 });
 
 app.listen(port, () => {
